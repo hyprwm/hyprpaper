1c19aa2b4f2b9e70dd9a3d2105f396cf094b0e82
Anton Samokhvalov <pg83@yandex.ru>
2023-01-05 00:53:44 +0300

    Update Hyprpaper.cpp (#31)

--- src/Hyprpaper.cpp
+++ src/Hyprpaper.cpp
@@ -199,7 +199,7 @@ void CHyprpaper::ensurePoolBuffersPresent() {
             if (m->size == Vector2D())
                 continue;
 
-            auto it = std::find_if(m_vBuffers.begin(), m_vBuffers.end(), [&](const std::unique_ptr<SPoolBuffer>& el) {
+            auto it = std::find_if(m_vBuffers.begin(), m_vBuffers.end(), [wt = wt, &m](const std::unique_ptr<SPoolBuffer>& el) {
                 return el->target == wt.m_szPath && el->pixelSize == m->size * m->scale;
             });
 
