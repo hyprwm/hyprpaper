275ebf0640f480fdd5f19a47897e35ca3fc70346
Mihai Fufezan <mihai@fufexan.net>
2026-03-02 18:32:53 +0200

    treewide: alejandra -> nixfmt

--- flake.nix
+++ flake.nix
@@ -58,58 +58,70 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-    ...
-  } @ inputs: let
-    inherit (nixpkgs) lib;
-    eachSystem = lib.genAttrs (import systems);
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
 
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        localSystem.system = system;
-        overlays = with self.overlays; [hyprpaper];
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem.system = system;
+          overlays = with self.overlays; [ hyprpaper ];
+        }
+      );
+      mkDate =
+        longDate:
+        (lib.concatStringsSep "-" [
+          (builtins.substring 0 4 longDate)
+          (builtins.substring 4 2 longDate)
+          (builtins.substring 6 2 longDate)
+        ]);
+      version = lib.removeSuffix "\n" (builtins.readFile ./VERSION);
+    in
+    {
+      overlays = {
+        default = self.overlays.hyprpaper;
+        hyprpaper = lib.composeManyExtensions [
+          inputs.aquamarine.overlays.default
+          inputs.hyprgraphics.overlays.default
+          inputs.hyprlang.overlays.default
+          inputs.hyprutils.overlays.default
+          inputs.hyprwayland-scanner.overlays.default
+          inputs.hyprtoolkit.overlays.default
+          inputs.hyprwire.overlays.default
+          (final: prev: rec {
+            hyprpaper = final.callPackage ./nix/default.nix {
+              stdenv = final.gcc15Stdenv;
+              version =
+                version
+                + "+date="
+                + (mkDate (self.lastModifiedDate or "19700101"))
+                + "_"
+                + (self.shortRev or "dirty");
+              commit = self.rev or "";
+            };
+            hyprpaper-debug = hyprpaper.override { debug = true; };
+          })
+        ];
+      };
+
+      packages = eachSystem (system: {
+        default = self.packages.${system}.hyprpaper;
+        inherit (pkgsFor.${system}) hyprpaper hyprpaper-debug;
       });
-    mkDate = longDate: (lib.concatStringsSep "-" [
-      (builtins.substring 0 4 longDate)
-      (builtins.substring 4 2 longDate)
-      (builtins.substring 6 2 longDate)
-    ]);
-    version = lib.removeSuffix "\n" (builtins.readFile ./VERSION);
-  in {
-    overlays = {
-      default = self.overlays.hyprpaper;
-      hyprpaper = lib.composeManyExtensions [
-        inputs.aquamarine.overlays.default
-        inputs.hyprgraphics.overlays.default
-        inputs.hyprlang.overlays.default
-        inputs.hyprutils.overlays.default
-        inputs.hyprwayland-scanner.overlays.default
-        inputs.hyprtoolkit.overlays.default
-        inputs.hyprwire.overlays.default
-        (final: prev: rec {
-          hyprpaper = final.callPackage ./nix/default.nix {
-            stdenv = final.gcc15Stdenv;
-            version = version + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
-            commit = self.rev or "";
-          };
-          hyprpaper-debug = hyprpaper.override {debug = true;};
-        })
-      ];
+
+      homeManagerModules = {
+        default = self.homeManagerModules.hyprpaper;
+        hyprpaper = builtins.throw "hyprpaper: the flake HM module has been removed. Use the module from Home Manager upstream.";
+      };
+
+      formatter = eachSystem (system: pkgsFor.${system}.nixfmt-tree);
     };
-
-    packages = eachSystem (system: {
-      default = self.packages.${system}.hyprpaper;
-      inherit (pkgsFor.${system}) hyprpaper hyprpaper-debug;
-    });
-
-    homeManagerModules = {
-      default = self.homeManagerModules.hyprpaper;
-      hyprpaper = builtins.throw "hyprpaper: the flake HM module has been removed. Use the module from Home Manager upstream.";
-    };
-
-    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
-  };
 }
